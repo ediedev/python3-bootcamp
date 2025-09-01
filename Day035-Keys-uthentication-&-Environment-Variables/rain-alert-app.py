d98761e4407cc81886dc1916d43bb02612a9562b
edtorto <edwardmtsa@gmail.com>
2025-09-01 20:49:55 +0300

    Remove Twilio secrets and load from environment Remove Twilio secrets and load from environment

--- Day035-Keys-uthentication-&-Environment-Variables/rain-alert-app.py
+++ Day035-Keys-uthentication-&-Environment-Variables/rain-alert-app.py
@@ -3,25 +3,21 @@ from twilio.rest import Client
 import os
 
 # Create environment variable
-os.environ["MY_APPID"] = "78ea50d7b0c4783f2516577959643fff"
-os.environ["TWILIO_ACCOUNT_SID"] = "ACcbadacc7f646347edf16696b8d191c3e"
 # latlong.net
 MY_LAT = 45.189505
 MY_LON = 11.604997
-account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
-auth_token = "74661cb0f513955e363cd208f03e9430"
 
 # openweathermap
 forecast_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-my_appid = os.environ.get("MY_APPID")# Access environment variable
-
+my_appid = os.environ.get("MY_APPID")
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 parameters = {
     "lat":MY_LAT,
     "lon":MY_LON,
     "appid":my_appid,
 }
-response = requests.get(forecast_endpoint, params=parameters)
-weather_data = response.json()
+weather_data = requests.get(forecast_endpoint, params=parameters).json()
 
 will_rain = False
 weather_slice = weather_data["list"][:12]
